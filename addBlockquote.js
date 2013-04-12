bb5cc86527b598e23386f9bc69172eb27934d364
Chris Dolphin <cdolphin1991@gmail.com>
2013-04-11 23:24:39 -0300

    Allowed for blockquotes to include html (i.e. links)
    Switched "text" from span tag to element "html" to allow for links to  be included in blockquotes.

--- addBlockquote.js
+++ addBlockquote.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 $("span").each(function() {
 
 if($(this).hasClass("callout-right")){
-   var $spantext = $(this).text();
+   var $spantext = $(this).html();
    var $parentp = $(this).parent();
 
-   $("<blockquote></blockquote>").prependTo($parentp).addClass("quote right").text($spantext);
+   $("<blockquote></blockquote>").prependTo($parentp).addClass("quote right").html($spantext);
 }
 if($(this).hasClass("callout-left")){
-   var $spantext = $(this).text();
+   var $spantext = $(this).html();
    var $parentp = $(this).parent();
 
-   $("<blockquote></blockquote>").prependTo($parentp).addClass("quote left").text($spantext);
+   $("<blockquote></blockquote>").prependTo($parentp).addClass("quote left").html($spantext);
 }
 
 });
